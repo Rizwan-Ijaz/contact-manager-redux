d024b49ecac076a93f39cde07874868855cb9f75
rizwan <rizwan.ijaz@dmechs.com>
2019-12-23 00:47:11 +0500

    CRUD completed with axios and redux

--- src/actions/contact.actions.js
+++ src/actions/contact.actions.js
@@ -5,23 +5,46 @@ const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 export const getContacts = () => async dispatch => {
     const response = await axios.get(API_URL);
-
     dispatch({
         type: types.GET_CONTACTS,
         payload: response.data
     });
 };
 
-export const addContact = (contact) => {
-    return {
-        type: types.ADD_CONTACTS,
-        payload: contact
-    }
+export const getContact = (id) => async dispatch => {
+    const response = await axios.get(`${API_URL}/${id}`);
+    dispatch({
+        type: types.GET_CONTACT,
+        payload: response.data
+    });
 };
 
-export const deleteContact = (id) => {
-    return {
-        type: types.DELETE_CONTACTS,
-        payload: id
+export const addContact = (contact) => async dispatch => {
+    const response = await axios.post(API_URL, contact);
+    dispatch({
+        type: types.ADD_CONTACT,
+        payload: response.data
+    });
+};
+
+export const deleteContact = (id) => async dispatch => {
+    try {
+        await axios.delete(`${API_URL}/${id}`);
+        dispatch({
+            type: types.DELETE_CONTACT,
+            payload: id
+        });
+    } catch (e) {
+        dispatch({
+            type: types.DELETE_CONTACTS,
+            payload: id
+        });
     }
+};
+export const updateContact = (contact) => async dispatch => {
+    const response = await axios.put(`${API_URL}/${contact.id}`, contact);
+    dispatch({
+        type: types.UPDATE_CONTACT,
+        payload: response.data
+    });
 };
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,7 +1,8 @@
 import types from "../actions/types";
 
 const initialState = {
-    contacts: []
+    contacts: [],
+    contact: {}
 };
 
 export default function (state = initialState, action) {
@@ -12,13 +13,25 @@ export default function (state = initialState, action) {
                 contacts: action.payload
             };
 
-        case types.ADD_CONTACTS:
+        case types.GET_CONTACT:
+            return {
+                ...state,
+                contact: action.payload
+            };
+
+        case types.UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => contact.id === action.payload.id ? (contact = action.payload) : contact)
+            };
+
+        case types.ADD_CONTACT:
             return {
                 ...state,
                 contacts: [action.payload, ...state.contacts]
             };
 
-        case types.DELETE_CONTACTS:
+        case types.DELETE_CONTACT:
             return {
                 ...state,
                 contacts: state.contacts.filter(contact => contact.id !== action.payload)
