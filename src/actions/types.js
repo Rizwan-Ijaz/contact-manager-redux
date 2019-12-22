fe254d19f7fe758132b40552c5b6e9e03424a55c
rizwan <rizwan.ijaz@dmechs.com>
2019-12-22 23:27:34 +0500

    Contacts Component is connected with redux

---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+const types = {
+    GET_CONTACTS: 'GET_CONTACTS'
+};
+
+export default types;
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,7 +1,15 @@
-const initialState = {};
+import types from "../actions/types";
+
+const initialState = {
+    contacts: []
+};
 
 export default function (state = initialState, action) {
     switch (action.type) {
+        case types.GET_CONTACTS:
+            return {
+                ...state
+            };
         default:
             return state;
     }
